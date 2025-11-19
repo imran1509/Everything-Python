c2934216fd21452a3a688533215093cfd3c122fd
imran1509 <iimran2508@gmail.com>
2025-11-19 21:00:06 +0530

    add: modules, pip and comments

---
+++ Chapter 1/module.py
@@ -0,0 +1,14 @@
+import pyjokes
+
+# This line is an example of a single line comment
+
+"""
+This is
+an 
+example of
+multi-line comments
+"""
+
+print("printing jokes...")
+joke = pyjokes.get_joke()
+print(joke)
