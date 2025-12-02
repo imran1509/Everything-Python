470d38ddfd339dec9570060715b8e8a5a78ab51d
imran1509 <iimran2508@gmail.com>
2025-12-02 18:15:43 +0530

    add: chapter 4 - practice sets

---
+++ Chapter 4 - practice sets/03_problem3.py
@@ -0,0 +1,5 @@
+# Check that a tuple type cannot be changed in python
+
+a = (34, 234, "Imran")
+
+a[2] = "Fmran"
---
+++ Chapter 4 - practice sets/04_problem4.py
@@ -0,0 +1,5 @@
+# Write a program to sum a list with 4 numbers
+
+l = [69, 34, 45, 245]
+
+print(sum(l))
