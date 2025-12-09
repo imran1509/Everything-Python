984ff8fcc94dacfdfd1fa4e7ffa5b3046993cc4d
imran1509 <iimran2508@gmail.com>
2025-12-09 19:49:41 +0530

    Add: Chapter 7 - practice sets

---
+++ Chapter 7 - practice sets/03_problem3.py
@@ -0,0 +1,10 @@
+# Attempt problem 1 using while loop
+# Write a program to print multiplication table of a given number using for loop.
+
+n = int(input("Enter a number: "))
+
+i = 1
+
+while(1<11):
+    print(f"{n} X {i} = {n * i}")
+    i += 1
