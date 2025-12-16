91d187d2e056cb3f95cc5509bda0f2a2e0304bf0
imran1509 <iimran2508@gmail.com>
2025-12-16 12:41:22 +0530

    Add: Chapter 8 - practice set

---
+++ Chapter 8 - pactice sets/07_problem7.py
@@ -0,0 +1,13 @@
+# Write a python function to remove a given word from a list and strip it at the same
+# time.
+
+def rem(l, word):
+    n = []
+    for item in l:
+        if not(item == word):
+            n.append(item.strip(word))
+        return n
+
+l = ["Imran", "Rohan", "Shubham", "an"]
+
+print(rem(l, "am"))
